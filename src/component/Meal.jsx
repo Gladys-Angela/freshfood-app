305a902cbd6a1ce460f506e6cf2b54b5a7a10a86
Gladys-Angela <gladysangelakamau@gmail.com>
2022-07-28 00:03:13 +0300

    enter

--- src/component/Meal.jsx
+++ src/component/Meal.jsx
@@ -6,6 +6,7 @@ const Meal = () => {
   const [url,setUrl]=useState("https:/www.themealdb.com/api/json/v1/1/search.php?f=a");
   const [item,setItem]=useState();
   const [show, setShow]=useState(false);
+  const [search, setSearch]=useState("");
   useEffect(()=>{
     fetch(url).then(res=>res.json()).then(data=>{
       console.log(data.meals);
@@ -18,6 +19,11 @@ const Meal = () => {
   const setIndex=(alpha)=>{
     setUrl(`https:/www.themealdb.com/api/json/v1/1/search.php?f=${alpha}`)
   }
+  const searchRecipe=(evt)=>{
+    if(evt.key==="Enter"){
+      setUrl(`https:/www.themealdb.com/api/json/v1/1/search.php?s=${search}`)
+    }
+  }
  
   return (
     <>
@@ -27,7 +33,8 @@ const Meal = () => {
         <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae adipisci odit dolorum molestias officia.</h4>
       </div>
       <div className="searchBox">
-        <input type="search" className="search-bar" />
+        <input type="search" className="search-bar"
+        onChange={e=>setSearch(e.target.value)} onKeyPress={searchRecipe}/>
       </div>
       <div className="container">
                {

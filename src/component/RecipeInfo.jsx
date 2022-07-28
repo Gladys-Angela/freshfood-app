d3e5ac8d872951daaf836a98d140d637a6bb35ae
Gladys-Angela <gladysangelakamau@gmail.com>
2022-07-28 03:08:50 +0300

    step

--- src/component/RecipeInfo.jsx
+++ src/component/RecipeInfo.jsx
@@ -1,9 +1,37 @@
 import React from 'react'
+import { useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 const RecipeInfo = () => {
+    const [item,setItem]=useState();
+    const {MealId}=useParams();
+    if(MealId!=="")
+    {
+        fetch(`https:/www.themealdb.com/api/json/v1/1/lookup.php?i=${MealId}`)
+        .then (res=>res.json())
+        .then(data=>{
+            setItem(data.meals[0]);
+        })
+    }
     return (
-        <>
-        <h3>Find your Recipe Information here</h3>
+        <>{
+        (!item)?"":(<>
+        <div className="content">
+            <img src={item.strMealThumb} alt="" />
+            <div className="innercontent">
+                <h1>{item.strMeal}</h1>
+                <h2>{item.strArea} Food</h2>
+                <h3>Category {item.strCategory}</h3>
+            </div>
+        </div>
+        <div className="recipe-details">
+            <div className="ingredients">
+                <h2>Ingredients</h2>
+                <h4>{item.strIngredient1}:{item.strMeasure1}</h4>
+            </div>
+        </div>
+        </>)
+}
         </>
     )
 }

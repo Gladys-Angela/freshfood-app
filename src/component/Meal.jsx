9686a715be24c840bbb0d896a24faece87c081b4
Gladys-Angela <gladysangelakamau@gmail.com>
2022-08-01 00:27:56 +0300

    api error

--- src/component/Meal.jsx
+++ src/component/Meal.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 import MealItem from './MealItem';
 import RecipeIndex from './RecipeIndex';
 const Meal = () => {
-  const [url,setUrl]=useState("https:/thawing-depths-16131.herokuapp.com/meals");
+  const [url,setUrl]=useState("https:/www.themealdb.com/api/json/v1/1/search.php?f=a");
   const [item,setItem]=useState();
   const [show, setShow]=useState(false);
   const [search, setSearch]=useState("");

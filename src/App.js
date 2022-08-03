581b871a5e4cdda7fabfba7421f65a42efbe39ec
Gladys-Angela <gladysangelakamau@gmail.com>
2022-08-03 13:20:41 +0300

    errorfix

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import "./component/style.css";
-import Meal from './component/Meal';
-import {Routes,Route} from "react-router-dom";
-import RecipeInfo from './component/RecipeInfo';
+import './Components/style.css';
+import Meal from './Components/Meal';
+import Recipe from './Components/Recipe';
+import {Route,Routes} from 'react-router-dom';
+
 function App() {
   return (
     <>
-    <Routes>
-    <Route path="/" element={<Meal/>}/>
-    <Route path="/:MealId" element={<RecipeInfo/>}/>
-    </Routes>
+   <Routes>
+      <Route  path="/" element={<Meal/>}/>
+      <Route exact path="/:recipeId" element={<Recipe/>}/>
+      </Routes>
     </>
-  );
+  )
 }
 
 export default App;
